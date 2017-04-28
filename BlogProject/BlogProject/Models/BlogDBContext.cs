037019553e6303f4dc89cbbcb31ff1fd12e4dd49
AleksandarKostadinov <aleksandarkostadinov555@gmail.com>
2017-04-28 14:36:46 +0300

    Added DbSet of ArticleComments in BlogDbContext.cs Model.

--- BlogProject/BlogProject/Models/BlogDBContext.cs
+++ BlogProject/BlogProject/Models/BlogDBContext.cs
@@ -12,6 +12,8 @@
 
         public DbSet<Article> Articles { get; set; }
 
+        public DbSet<ArticleComment> Comments { get; set; }
+
         public static BlogDbContext Create()
         {
             return new BlogDbContext();
